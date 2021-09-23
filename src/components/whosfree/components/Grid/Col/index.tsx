8124ecc6c4f76b2b90eba616ab10a1881396d3b1
mydatahack <43569574+mydatahack@users.noreply.github.com>
2021-09-23 14:30:31 +1000

    Reorganised folder structure (#6)
    * Reorganised folder structure
    
    * Update linting...

--- src/components/whosfree/components/Grid/Col/index.tsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-
-export const Col = () => <div>Col</div>;
--- src/components/whosfree/utils/index.ts
+++
@@ -1,1 +0,0 @@
-export const joinClassNames = (...classes: Array<string | boolean>) => classes.filter((element) => typeof element === 'string').join(' ');
