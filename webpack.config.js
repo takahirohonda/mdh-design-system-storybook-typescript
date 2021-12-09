e34e0a83a342167bdaffdfad9b743488cc868416
mydatahack <thon6thon6@hotmail.com>
2021-12-10 08:27:38 +1100

    fix: doc update testing pipeline

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const path = require('path')
+const path = require('path');
+
 const devMode = process.env.NODE_ENV !== 'production';
 
 const cssLoader = {
@@ -8,65 +9,63 @@ const cssLoader = {
   },
 };
 
-module.exports = (env, argv) => {
-  return {
-    mode: argv.mode || 'development',
-    entry: {
-      main: './src/index.ts',
-      // can have multiple entry
-      // register: './src/register/Index.tsx'
-    },
-    output: {
-      filename: 'bundle.js',
-      path: path.join(__dirname, '/dist')
-    },
-    module: {
-      rules: [
-        {
-          test: /\.ts(x?)$/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: 'ts-loader',
-            },
-          ],
-        },
-        {
-          test: /\.css$/,
-          use: ['style-loader', cssLoader],
-        },
-        {
-          test: /src.+\.scss$/,
-          loaders: [
-            'style-loader',
-            'css-modules-typescript-loader',
-            {
-              ...cssLoader,
-              options: {
-                modules: true
-              },
-            },
-            'sass-loader',
-          ],
-        },
-        {
-          test: /img.+\.svg$/,
-          loader: 'svg-inline-loader?classPrefix',
-        },
-        {
-          test: /fonts.+\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-          use: [{
-            loader: 'file-loader',
+module.exports = (env, argv) => ({
+  mode: argv.mode || 'development',
+  entry: {
+    main: './src/index.ts',
+    // can have multiple entry
+    // register: './src/register/Index.tsx'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '/dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', cssLoader],
+      },
+      {
+        test: /src.+\.scss$/,
+        loaders: [
+          'style-loader',
+          'css-modules-typescript-loader',
+          {
+            ...cssLoader,
             options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts/',
+              modules: true,
             },
-          }],
-        },
-      ]
-    },
-    resolve: {
-      extensions: ['.js', '.ts', '.tsx'],
-    },
-  }
-}
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /img.+\.svg$/,
+        loader: 'svg-inline-loader?classPrefix',
+      },
+      {
+        test: /fonts.+\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/',
+          },
+        }],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+});
