d2f84157aabeee1464be539b5e4ba2802dbed9e3
mydatahack <thon6thon6@hotmail.com>
2022-03-18 16:28:51 +1100

    refactor(config): updating config

--- rollup.config.js
+++ rollup.config.js
@@ -39,7 +39,7 @@ export default {
       format: 'esm',
       sourcemap: false,
       preserveModules: true,
-      // preserveModulesRoot: 'src', // this will make d.ts files created in the separate folder. Without it, it creates d.ts files in the same folders.
+      preserveModulesRoot: 'src', // this will make d.ts files created in the separate folder. Without it, it creates d.ts files in the same folders.
     },
   ],
   external: [
@@ -47,12 +47,12 @@ export default {
     'react-select/async', // rollup can't tell that this is a node_module
   ],
   plugins: [
+    resolve(),
+    commonjs(),
     typescript({
       sourceMap: false,
       exclude: ['**/*.(test|spec).(ts|tsx)'], // don't generate *.d.ts files
     }),
-    resolve(),
-    commonjs(),
     postcss({
       modules: true,
     }),
